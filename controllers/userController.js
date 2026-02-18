dd2f1faa1b9b726b9e3aed78c26438b5b06e1e7d
Akhilesh Khatri <akhileshkhatri4@gmail.com>
2026-02-18 16:06:22 +0530

    Update cookie settings for production environment

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,8 +77,8 @@ export const login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: 'lax',
-                secure: false,
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+                secure: process.env.NODE_ENV === "production",
                 path: '/'
             })
             .json({
