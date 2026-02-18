6655c661133e949d0f3d6d0187daf945cc3f992e
Akhilesh Khatri <akhileshkhatri4@gmail.com>
2026-02-18 16:32:14 +0530

    Refactor code formatting and update CORS settings for consistency

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-import express from "express"; 
-import dotenv from "dotenv"; 
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config({});
 
- 
+
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
-const corsOption={
-    origin:'http://localhost:5173',
-    credentials:true
+const corsOption = {
+    origin: [
+        "http://localhost:5173",
+        "https://chat-application-frontend-jade.vercel.app"
+    ],
+    credentials: true
 };
-app.use(cors(corsOption)); 
+app.use(cors(corsOption));
 
 
 // routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-server.listen(PORT, ()=>{
+
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server listen at port ${PORT}`);
 });
--- socket/socket.js
+++ socket/socket.js
@@ -7,10 +7,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173'],
-        methods: ['GET', 'POST'],
+        origin: [
+            "http://localhost:5173",
+            "https://chat-application-frontend-jade.vercel.app"
+        ],
+        methods: ["GET", "POST"],
+        credentials: true
     },
 });
+
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
